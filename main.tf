7dd50da23c8a2e23fcbdbe131b359576425b2ec4
Travinsel <travinsel@gmail.com>
2024-05-15 00:49:38 +0000

    files

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region                   = "us-east-2"  # Specifies the AWS region
-  shared_credentials_files = ["~/.aws/credentials"]  # Specifies the location of the AWS shared credentials file
-  profile                  = "tfuser"  # Specifies the AWS profile to use from the shared credentials file
+  region                   = "us-east-2"  
+  shared_credentials_files = ["~/.aws/credentials"]  
 }
