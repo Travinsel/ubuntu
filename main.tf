6221b04e42c8c0ff16aec42a6152851057bb4869
Travinsel <travinsel@gmail.com>
2024-05-18 14:57:10 +0000

    updates

--- main.tf
+++ main.tf
@@ -2,11 +2,31 @@ provider "aws" {
   region                   = "us-east-2"
   shared_credentials_files = ["~/.aws/credentials"]
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "main_subnet"
+  }
+}
+
 resource "aws_instance" "tfvm2" {
   ami                    = "ami-02bf8ce06a8ed6092"
   instance_type          = "t2.micro"
   key_name               = "Linux"
   availability_zone      = "us-east-2a"
+  subnet_id              = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.allow.id]
 
   tags = {
@@ -16,7 +36,9 @@ resource "aws_instance" "tfvm2" {
 
 resource "aws_security_group" "allow" {
   name        = "vpc"
-  description = ""
+  description = "Allow HTTP inbound traffic"
+
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "allow_http"
@@ -29,7 +51,7 @@ resource "aws_security_group_rule" "allow_http_ingress" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.main.cidr_block] # Assuming you have defined 'aws_vpc' resource
+  cidr_blocks       = [aws_vpc.main.cidr_block] # Reference to the VPC CIDR block
 }
 
 resource "aws_security_group_rule" "allow_all_egress" {
--- s3.tf
+++ s3.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "tfbucket1" {
-  bucket = "tktf_state-bucket"
+  bucket = "tktf-state-bucket"
+
+  tags = {
+    Name = "tktf-state-bucket"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "tfbucket1vers" {
